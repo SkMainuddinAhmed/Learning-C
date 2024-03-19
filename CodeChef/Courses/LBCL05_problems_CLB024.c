9c34c9dce564bbf8227b720c87021c1384cd8832
SkMainuddinAhmed <skmainuddinahmed@gmail.com>
2024-03-19 11:05:35 +0530

    codechef course 15th code

---
+++ CodeChef/Courses/LBCL05_problems_CLB024.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	// Assign the value 23 to a
+int a=23 , b=20;
+    
+    // Assign the value 20 to b
+
+    
+    // Output the sum of a and b
+    printf("%d",a+b);
+}
+
