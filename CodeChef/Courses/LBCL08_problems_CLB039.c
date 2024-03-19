cdc20cbfd629aa6cb7bafe4d3fd58cdc78fb0478
SkMainuddinAhmed <skmainuddinahmed@gmail.com>
2024-03-19 12:12:49 +0530

    codechef course 18th code

---
+++ CodeChef/Courses/LBCL08_problems_CLB039.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main () {
+
+    char x[] = "Hello";
+    
+    // declare string y with "World"
+    
+    // concatenate the two strings
+    
+    // print the final string
+    char y[] = "World";
+  strcat(x, y);
+    printf("%s", x);
+
+}
